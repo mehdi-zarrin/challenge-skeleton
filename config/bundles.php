663d4e66a98afd34f0223a13236427bd8f2258e8
Mehdi <mehdi.zarrin@gmail.com>
2022-09-14 01:33:29 +0200

    updgrade symfony to version 6, added alice fixture support, fix behat context abandoned repo issue by forking into github and change the compatible version of symfony (still need to be tested), fix missing parameters in the .env.dist file.

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
